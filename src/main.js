32e147beaea9346369bf4ac3f4a1e07d6c5dabbc
seb <sebastien.benard@kujjuk-group.com>
2017-03-14 23:00:52 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
-import Form from "./components/Form";
-import Door from "./components/Door";
 import Vis from "./components/Vis";
 import Storm from "./components/Storm";
 
@@ -13,19 +11,9 @@ const target = document.getElementById('root');
 
 const node = (
     <App>
-        <Form/>
-        <Door/>
         <Vis/>
         <Storm/>
     </App>
 );
-/*
- <App>
 
- <Storm/>
- </App>
- */
-/*<Form/>
- <Door/>
- <Vis/>*/
 ReactDOM.render(node, target);
